a8faf558c56fd1f9e2ca0fb69322ca8af465c2c1
Klaudia Gancarczyk <klaudia.gancarczyk6@gmail.com>
2023-07-13 14:37:52 +0200

    Add more header styles

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-console.log('ok')
+const burger = document.querySelector('.burger');
+const sections = document.querySelectorAll('section');
+
+burger.addEventListener("click", ()=> {
+    if(burger.classList.contains("active")){
+        gsap.to(".nav-links", {x: "100%"});
+        gsap.to(".burger", {color: "var(--light)"});
+        gsap.set("body", {overflow: "auto"})
+    }else{
+        gsap.to(".nav-links", {x: "0%"});
+        gsap.to(".burger", {color: "var(--dark)"});
+        gsap.set("body", {overflow: "hidden"})
+    }
+    burger.classList.toggle("active");
+
+});
+
+
